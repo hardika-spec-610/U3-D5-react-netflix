d22e71a6fecead3cca7e26d09d4b8b4afbce739f
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-02-08 14:54:15 +0100

    install react router dom

--- src/App.js
+++ src/App.js
@@ -8,27 +8,34 @@ import Gallery1 from "./Component/Gallery1";
 import TvShows from "./Component/TvShows";
 import Footer from "./Component/Footer";
 import "./Component/componentStyles.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound404 from "./Component/NotFound404";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <NavbarComponent logo={netflixLogo} accountLogo={accountLogo} />
-      </header>
-      <Container fluid className="px-5 pb-5">
-        <TvShows />
-        <Gallery1 name="harry potter" title="Harry Potter" />
-        <Gallery1 name="Lord of the Rings" title="Lord of the Rings" />
-        <Gallery1 name="Titanic" title="Titanic" />
-        <Gallery1 name="Back to the Outback" title="Back to the Outback" />
-        <Gallery1 name="Squid Game" title="Squid Game" />
-      </Container>
-      <footer className="ftr-block">
-        <Container fluid className="px-5">
-          <Footer />
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <NavbarComponent logo={netflixLogo} accountLogo={accountLogo} />
+        </header>
+        <Container fluid className="px-5 pb-5">
+          <Routes>
+            <Route element={<TvShows />} path="/tv-shows" />
+            <Route element={<NotFound404 />} path="*" />
+          </Routes>
+          <Gallery1 name="harry potter" title="Harry Potter" />
+          <Gallery1 name="Lord of the Rings" title="Lord of the Rings" />
+          <Gallery1 name="Titanic" title="Titanic" />
+          <Gallery1 name="Back to the Outback" title="Back to the Outback" />
+          <Gallery1 name="Squid Game" title="Squid Game" />
         </Container>
-      </footer>
-    </div>
+        <footer className="ftr-block">
+          <Container fluid className="px-5">
+            <Footer />
+          </Container>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Component/NavbarComponent.jsx
+++ src/Component/NavbarComponent.jsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import {
   Container,
   Dropdown,
@@ -11,68 +10,73 @@ import {
 import "../App";
 import { BsSearch, BsBellFill } from "react-icons/bs";
 import "../Component/componentStyles.css";
+import { Link, useLocation } from "react-router-dom";
 
-class NavbarComponent extends Component {
-  render() {
-    return (
-      <Navbar className="navbar navbar-dark nav-bg px-0" expand="lg">
-        <Container fluid className="px-5">
-          <Navbar.Brand href="#home">
-            <img src={this.props.logo} alt="netflix logo" width="110px" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Tv Shows</Nav.Link>
-              <Nav.Link href="#link">Movies</Nav.Link>
-              <Nav.Link href="#link">Recently Added</Nav.Link>
-              <Nav.Link href="#link">My List</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <Row className="d-flex align-items-center">
-                <BsSearch />
-                <Nav.Link href="#kids">KIDS</Nav.Link>
-                <BsBellFill />
-                <Dropdown className="btn-group">
-                  <Dropdown.Toggle className="dropdown-btn" id="dropdown-basic">
-                    <img
-                      src={this.props.accountLogo}
-                      alt="kids icon"
-                      width="24px"
-                    />
-                  </Dropdown.Toggle>
+const NavbarComponent = ({ logo, accountLogo }) => {
+  const location = useLocation();
+  console.log("location object:", location);
+  return (
+    <Navbar className="navbar navbar-dark nav-bg px-0" expand="lg">
+      <Container fluid className="px-5">
+        <Navbar.Brand>
+          <img src={logo} alt="netflix logo" width="110px" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>Home</Nav.Link>
+            <Link to="/tv-shows">
+              <span
+                className={
+                  location.pathname === "/tv-shows"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Tv Shows
+              </span>
+            </Link>
+            <Nav.Link>Movies</Nav.Link>
+            <Nav.Link>Recently Added</Nav.Link>
+            <Nav.Link>My List</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <Row className="d-flex align-items-center">
+              <BsSearch />
+              <Nav.Link href="#kids">KIDS</Nav.Link>
+              <BsBellFill />
+              <Dropdown className="btn-group">
+                <Dropdown.Toggle className="dropdown-btn" id="dropdown-basic">
+                  <img src={accountLogo} alt="kids icon" width="24px" />
+                </Dropdown.Toggle>
 
-                  <Dropdown.Menu className="dropdown-menu dropdown-menu-right">
-                    <Dropdown.Item href="#">Edit Profile</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">
-                      Another action
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
-                      Account Settings
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Row>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-}
+                <Dropdown.Menu className="dropdown-menu dropdown-menu-right">
+                  <Dropdown.Item href="#">Edit Profile</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Account Settings
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Row>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavbarComponent;
