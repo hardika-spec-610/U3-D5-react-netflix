81ecc7667dd18880562324acbc351438e5b20aee
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-02-09 13:18:36 +0100

    style correction

--- src/Component/CarouselItem.jsx
+++ src/Component/CarouselItem.jsx
@@ -4,33 +4,35 @@ import { Link } from "react-router-dom";
 
 const CarouselItem = ({ id, poster, title, movieType, movieYear, movieId }) => {
   return (
-    <Col key={id} xs={12} sm={6} md={3} lg={2} className="px-1 mb-4 movie-card">
-      <img
-        className="d-block w-100"
-        src={poster}
-        alt="Movie Poster"
-        width="300px"
-        height="310px"
-      />
-      <div className="infos-container">
-        <div className="d-flex mb-3 mt-4 align-items-center">
-          <BsPlayCircle className="mr-2" />
+    <Col key={id} xs={12} sm={6} md={3} lg={2} className="px-1 mb-4 ">
+      <div className="movie-card">
+        <img
+          className="d-block w-100"
+          src={poster}
+          alt="Movie Poster"
+          width="300px"
+          height="310px"
+        />
+        <div className="infos-container">
+          <div className="d-flex mb-3 mt-4 align-items-center">
+            <BsPlayCircle className="mr-2" />
 
-          <span>Play S8 E7</span>
-          <BsPlus className="ml-auto plus-btn" />
-        </div>
-        <h6 className="movie-title text-left">{title}</h6>
+            <span>Play S8 E7</span>
+            <BsPlus className="ml-auto plus-btn" />
+          </div>
+          <h6 className="movie-title text-left">{title}</h6>
 
-        <div className="card-movie-footer d-flex align-items-center">
-          <span className="mr-2">{movieType}</span>
-          <span className="mr-2">{movieYear}</span>
-          <BsChatSquareText className="mr-2" />
+          <div className="card-movie-footer d-flex align-items-center">
+            <span className="mr-2">{movieType}</span>
+            <span className="mr-2">{movieYear}</span>
+            <BsChatSquareText className="mr-2" />
+          </div>
+          <Link to={"/details/" + movieId}>
+            <Button variant="dark" className="m-btn py-1 mt-3">
+              More Details
+            </Button>
+          </Link>
         </div>
-        <Link to={"/details/" + movieId}>
-          <Button variant="dark" className="m-btn py-1 mt-3">
-            More Details
-          </Button>
-        </Link>
       </div>
     </Col>
   );
--- src/Component/EditProfile.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class EditProfile extends Component {
-  render() {
-    return <></>;
-  }
-}
-
-export default EditProfile;
